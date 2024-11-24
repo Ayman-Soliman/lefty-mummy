209aefe8c17bc6f89f2c130bf7b5da96740c252c
Ayman-Soliman <Ayman-Soliman@github.com>
2024-11-24 21:29:43 +0200

    edit

--- api/config.php
+++ api/config.php
@@ -14,19 +14,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 // echo json_encode(["message" => "CORS is configured!"]);
 
 // config.php: Database configuration
-if ($_SERVER['HTTP_HOST'] == 'localhost:8080' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
-    // Localhost environment
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'lm_ecom');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-} else {
-    // Live server environment
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'leftpsno_lm_ecom');
-    define('DB_USER', 'leftpsno_lefty-mummy');
-    define('DB_PASS', 'omar-hamza-ali-2011-2015-2019');
-}
+// if ($_SERVER['HTTP_HOST'] == 'localhost:8080' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+//     // Localhost environment
+//     define('DB_HOST', 'localhost');
+//     define('DB_NAME', 'lm_ecom');
+//     define('DB_USER', 'root');
+//     define('DB_PASS', '');
+// } else {
+//     // Live server environment
+//     define('DB_HOST', 'localhost');
+//     define('DB_NAME', 'leftpsno_lm_ecom');
+//     define('DB_USER', 'leftpsno_lefty-mummy');
+//     define('DB_PASS', 'omar-hamza-ali-2011-2015-2019');
+// }
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'leftpsno_lm_ecom');
+define('DB_USER', 'leftpsno_lefty-mummy');
+define('DB_PASS', 'omar-hamza-ali-2011-2015-2019');
 
 try {
     $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
