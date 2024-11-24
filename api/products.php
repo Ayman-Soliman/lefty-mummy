52f421cefc0fb458ca40484785fdea1d679b1f0a
Ayman-Soliman <Ayman-Soliman@github.com>
2024-11-24 21:53:48 +0200

    edit get products

--- api/products.php
+++ api/products.php
@@ -2,7 +2,7 @@
 require 'config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $query = "SELECT * FROM 'products' ";
+    $query = "SELECT * FROM products ";
     $query_run = mysqli_query($con, $query);
     // return $query_run;
     echo json_encode($query_run);
