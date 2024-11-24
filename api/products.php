b01ce9bfba88a561d8a74022fe6f6379bce166d6
Ayman-Soliman <Ayman-Soliman@github.com>
2024-11-24 21:57:22 +0200

    edit query

--- api/products.php
+++ api/products.php
@@ -4,6 +4,14 @@ require 'config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $query = "SELECT * FROM products ";
     $query_run = mysqli_query($con, $query);
-    // return $query_run;
-    echo json_encode($query_run);
+    if ($query_run) {
+        $data = []; // Array to store results
+        while ($row = mysqli_fetch_assoc($query_run)) {
+            $data[] = $row; // Append each row to the array
+        }
+    
+        echo json_encode($data); // Encode the array into JSON format
+    } else {
+        echo json_encode(["error" => "Query failed: " . mysqli_error($con)]);
+    }
 }
