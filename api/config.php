b44f4cad49b3b4aedc9ffbc4333b9343db570825
Ayman-Soliman <Ayman-Soliman@github.com>
2024-11-24 21:50:31 +0200

    dispaly php errors

--- api/config.php
+++ api/config.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // CORS headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
